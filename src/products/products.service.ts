983eaab46ae3af75995e2f2ced601129cd34c855
Sanjeev Kumar Singh <sanjeev.singh@napses.com>
2024-07-30 16:44:41 +0530

    remove async wrapper

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -35,66 +35,37 @@ export class ProductsService {
     price: number,
     category: ProductCategory,
   ): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.productsRepository
-        .insert({
-          title: title,
-          description: description,
-          price: price,
-          category: category,
-        })
-        .then((product) => resolve(product))
-        .catch((err) => reject(err));
+    return this.productsRepository.insert({
+      title: title,
+      description: description,
+      price: price,
+      category: category,
     });
   }
 
   count(): Promise<number> {
-    return new Promise((resolve, reject) => {
-      this.productsRepository
-        .count()
-        .then((product) => resolve(product))
-        .catch((err) => reject(err));
-    });
+    return this.productsRepository.count();
   }
 
   findOne(id: number): Promise<Product> {
-    return new Promise((resolve, reject) => {
-      this.productsRepository
-        .findOne({
-          where: {
-            id: id,
-          },
-        })
-        .then((product) => resolve(product))
-        .catch((err) => reject(err));
+    return this.productsRepository.findOne({
+      where: {
+        id: id,
+      },
     });
   }
 
   findAndCount(id: number): Promise<unknown> {
-    return new Promise((resolve, reject) => {
-      this.productsRepository
-        .findAndCount({
-          where: {
-            id: id,
-          },
-        })
-        .then(([product, totalCount]) => {
-          return resolve([product, totalCount]);
-        })
-        .catch((err) => reject(err));
+    return this.productsRepository.findAndCount({
+      where: {
+        id: id,
+      },
     });
   }
 
   findAndCountBy(id: number): Promise<unknown> {
-    return new Promise((resolve, reject) => {
-      this.productsRepository
-        .findAndCountBy({
-          id: id,
-        })
-        .then(([product, totalCount]) => {
-          return resolve([product, totalCount]);
-        })
-        .catch((err) => reject(err));
+    return this.productsRepository.findAndCountBy({
+      id: id,
     });
   }
 }
