ced48e56ca627bc4e1a5683cc9eb7bcbb1f96135
Sanjeev Kumar Singh <sanjeev.singh@napses.com>
2024-07-30 14:32:02 +0530

    remove comment

--- src/products/products.controllers.ts
+++ src/products/products.controllers.ts
@@ -23,25 +23,4 @@ export class ProductsController {
   getProduct(@Param('id') productId: number) {
     return this.productsService.findOne(productId);
   }
-
-  // @Patch(':id')
-  // updateProduct(
-  //   @Param('id') prodId: string,
-  //   @Body('title') prodTitle: string,
-  //   @Body('description') prodDesc: string,
-  //   @Body('price') prodPrice: number,
-  // ) {
-  //   return this.productsService.updateProduct(
-  //     prodId,
-  //     prodTitle,
-  //     prodDesc,
-  //     prodPrice,
-  //   );
-  // }
-
-  // @Delete(':id')
-  // removeProduct(@Param('id') prodId: string) {
-  //   this.productsService.deleteProduct(prodId);
-  //   return null;
-  // }
 }
